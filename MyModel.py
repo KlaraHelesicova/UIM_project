2bb7870e0b4a08f5114834dc08a6062fceea4631
xsvest08 <61969702+xsvest08@users.noreply.github.com>
2021-11-28 14:31:44 +0100

    Add files via upload

---
+++ MyModel.py
@@ -0,0 +1,10 @@
+def MyModel(data):
+    '''
+
+    :param data: Do této funkce implementujte váš výsledný model pro klasifikaci/predikci a veškeré algoritmy
+    pro předzpracování dat
+    vstupní surová data reprezentující 1 objekt (1 pacienta, 1 obrázek, 1 časovou řadu)
+    :return: zařazení objektu do třídy
+    '''
+
+output = MyModel[]
