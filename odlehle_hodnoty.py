940f1c235787196e79a86adfa5485cd9f8cbf723
Marie Machalova <xplevk00@vutbr.cz>
2021-12-08 19:34:25 +0100

    revize 2

--- odlehle_hodnoty.py
+++ odlehle_hodnoty.py
@@ -1,7 +1,7 @@
 from sklearn import preprocessing
 import numpy as np
 import pandas as pd
-# ,2,3,4,5,6,7,8,9,10,11,12,13,14
+
 df = pd.read_csv('dataSepsis.csv',delimiter=';', usecols=[*range(0, 35), 38, 39])
 median = df.median()
 std = df.std()
@@ -11,7 +11,7 @@ df.fillna(median, inplace=True)
 
 
 
-x = df.values #returns a numpy array
+x = df.values
 min_max_scaler = preprocessing.MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(x)
 df = pd.DataFrame(x_scaled)
